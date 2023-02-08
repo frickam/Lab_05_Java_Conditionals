6c7430fcd65c193e86fc1ae9a58801e503df6b45
frickam <frickam@mail.uc.edu>
2023-02-08 10:30:13 -0500

    Initial commit

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ShipCostCalculator
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double item = 0;
+        double shipping = 0.02;
+        double total = 0;
+        
+    }
+}
